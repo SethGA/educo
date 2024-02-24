78d819eaa3527ed46c1c8e47b7be741a5242fc4e
Seth Grief-Albert <sethgriefalbert@gmail.com>
2024-02-24 11:08:45 -0500

    Implement basic chat functionality

--- Hello.py
+++ Hello.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import openai
+from openai import OpenAI
 # import pyperclip
 
 # st.set_page_config(
@@ -10,9 +11,9 @@ import openai
 st.title("Educo")
 # st.write("""Chat with gpt-4.""")
 # st.sidebar.success("Select an item above")
-
 OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 openai.api_key = OPENAI_API_KEY
+client = OpenAI()
 
 # def on_copy_click(text):
 #     st.session_state.copied.append(text)
@@ -20,7 +21,7 @@ openai.api_key = OPENAI_API_KEY
 
 # Set a model
 if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo" # gpt-3.5-turbo
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -37,7 +38,7 @@ for message in st.session_state.messages:
 
 if prompt := st.chat_input("What's up?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
-    
+
     # user
     with st.chat_message("user"):
         st.markdown(prompt)
@@ -47,16 +48,16 @@ if prompt := st.chat_input("What's up?"):
     with st.chat_message("assistant"):
         placeholder = st.empty()
         full_response = ""
-        for response in openai.ChatCompletion.create(
+        response = client.chat.completions.create(
             model=st.session_state["openai_model"],
             messages=[
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
             ],
-            stream=True,
-        ):
-            full_response += response.choices[0].delta.get("content", "")
-            placeholder.markdown(full_response + "▌")
+           #stream=True,
+        )
+        full_response += str(response.choices[0].message)
+        placeholder.markdown(full_response + "▌")
         placeholder.markdown(full_response)
     st.session_state.messages.append({"role": "assistant", "content": full_response})
     # BUTTON
